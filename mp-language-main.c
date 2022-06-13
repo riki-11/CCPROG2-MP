28d7aaacf1af32ac6d7ca49e056ecda8a595797f
Milkz03 <105696813+Milkz03@users.noreply.github.com>
2022-06-13 21:06:46 +0800

    Update mp-language-main.c

--- mp-language-main.c
+++ mp-language-main.c
@@ -3,7 +3,7 @@
 int
 main()
 {
-    int nMMInput = 0, nMLInput = 0, nEntryCount = 0;
+    int nMMInput = 0, nMLInput = 0, nEntryCount = 0, nInputElem = 0, nLineElem = 0, nFileWords = 0;
     entry aEntries[MAX_ENTRIES];
 
     initDatabase(aEntries); // Initializes database of entries
@@ -14,7 +14,7 @@ main()
         getMMInput(&nMMInput); // User chooses between MD and LT
 
         if (nMMInput != 3)
-            switchMainMenu(nMMInput, &nMLInput, aEntries, &nEntryCount); // Program processes user's input
+            switchMainMenu(nMMInput, &nMLInput, aEntries, &nEntryCount, &nInputElem, &nLineElem, &nFileWords); // Program processes user's input
         
     } while (nMMInput != 3);
 
